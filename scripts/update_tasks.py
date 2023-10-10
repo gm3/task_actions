ebf936801225823288341eff83d24a76005fac8e
boomboxhead <7612104+gm3@users.noreply.github.com>
2023-10-10 01:25:15 -0500

    Update update_tasks.py

--- scripts/update_tasks.py
+++ scripts/update_tasks.py
@@ -56,7 +56,7 @@ top_3_tasks = sorted_tasks[:3]
 formatted_tasks = [f"{task['amount']} | {task['name']} | Date Posted: {task['date_posted']}" for task in top_3_tasks]
 
 # Directory to save the text files
-output_directory = './automation/top_tasks/'
+output_directory = '/top_tasks/'
 
 # Ensure the output directory exists
 if not os.path.exists(output_directory):
